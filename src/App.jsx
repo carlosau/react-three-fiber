40389ffc4b1756b9f8972aebe80dcd2b22419fe7
Carlos Souza <carlosaugsz@hotmail.com>
2024-12-10 12:05:40 -0300

    box animation test

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
+import { Canvas, useFrame } from '@react-three/fiber'
+import { useRef } from 'react';
+
+function AnimatedBox() {
+  const boxRef = useRef()
+
+  useFrame(() => {
+    boxRef.current.rotation.x += 0.005;
+    boxRef.current.rotation.y += 0.005;
+    boxRef.current.rotation.z += 0.005;
+  })
+
+  return(
+    <mesh ref={boxRef}>
+      <boxGeometry args={[2.5, 2, 2.5]} />
+      <meshStandardMaterial color="blue" />
+    </mesh>
+  )
+}
+
 function App() {
   return (
-    <div>
-      React 3F
+    <div id='canvas-container'>
+      <Canvas>
+        <AnimatedBox />
+        <directionalLight position={[2, 5, 1]} />
+      </Canvas>
     </div>
   )
 }
