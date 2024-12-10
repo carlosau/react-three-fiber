61970d4436107409f36961164d1240c1b775eda1
Carlos Souza <carlosaugsz@hotmail.com>
2024-12-10 14:03:40 -0300

    tested leva controls

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,24 @@ import { Canvas, useFrame } from '@react-three/fiber'
 import { useRef } from 'react';
 import { OrbitControls } from '@react-three/drei';
 import { Image } from '@react-three/drei';
-
+import { useControls } from 'leva';
 
 function AnimatedBox() {
   const boxRef = useRef()
 
+  const { speed } = useControls({
+    speed: {
+      value: 0.005,
+      min: 0.0,
+      max: 0.03,
+      step: 0.001,
+    }
+  })
+
   useFrame(() => {
-    boxRef.current.rotation.x += 0.005;
-    boxRef.current.rotation.y += 0.005;
-    boxRef.current.rotation.z += 0.005;
+    boxRef.current.rotation.x += speed;
+    boxRef.current.rotation.y += speed;
+    boxRef.current.rotation.z += speed;
   })
     
 
